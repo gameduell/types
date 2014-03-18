71b1298301de9d3dd32695bd3f03b70254704e03
Rui Campos <rui.campos@gameduell.de>
2014-03-18 14:44:19 +0100

    INFRA-2899 Now setInt and setFloat instead of setValue (faster like this)

--- types/Data.hx
+++ types/Data.hx
@@ -8,9 +8,13 @@ extern Class Data
 
 	public function setData(data : Data, offsetInBytes : Int);
 
-	public function setValue(value : Dynamic, offsetInBytes : Int, dataType : DataType);
+	public function setInt(value : Int, offsetInBytes : Int, targetDataTyoe : DataType);
 
-	public function setArray(array : Array<Dynamic>, offsetInBytes : Int, dataType : DataType);
+	public function setFloat(value : Float, offsetInBytes : Int, targetDataTyoe : DataType);
+
+	public function setIntArray(array : Array<Int>, offsetInBytes : Int, dataType : DataType);
+
+	public function setFloatArray(array : Array<Float>, offsetInBytes : Int, dataType : DataType);
 
 	public function toString(?dataType : DataType) : String; /// defaults to DataType.Int
 
--- types/DataType.hx
+++ types/DataType.hx
@@ -10,3 +10,28 @@ enum DataType
 	UnsignedInt;
 	Float;
 }
+
+class DataTypeUtils
+{
+	static public function dataTypeByteSize(dataType : DataType) : Int
+	{
+		switch(dataType)
+		{
+			case Byte:
+				return 1; 
+			case UnsignedByte:
+				return 1;
+			case Short:
+				return 2;
+			case UnsignedShort:
+				return 2;
+			case Int:
+				return 4;
+			case UnsignedInt:
+				return 4;
+			case Float:
+				return 4;
+		}
+		return 0;
+	}
+}
