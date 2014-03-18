8680ec1ec94121c568212c86ba98ec3545c8c0bf
Rui Campos <rui.campos@gameduell.de>
2014-03-18 14:43:41 +0100

    INFRA-2899 Implemented data methods, missing matrix

--- types/Data.hx
+++ types/Data.hx
@@ -2,72 +2,220 @@ package types;
 
 import types.DataType;
 
- class Data
+@:headerCode("
+#include <string>
+#include <sstream>
+")
+
+@:cppFileCode('							
+static void staticClose(types::Data_obj *inObj) 		
+{ 										
+	if (inObj->_dataPointer)   			
+		free(inObj->_dataPointer); 							
+}	
+
+static inline std::wstring staticReadValueIntoString(void *pointer, types::DataType &dataType)
+{
+	std::wostringstream oss;
+	switch( (int)(dataType->__Index())){
+		case (int)0: {
+			oss << (int)(((int8_t*)pointer)[0]);
+		}
+		;break;
+		case (int)1: {
+			oss << (int)(((uint8_t*)pointer)[0]);
+		}
+		;break;
+		case (int)2: {
+			oss << (int)(((int16_t*)pointer)[0]);
+		}
+		;break;
+		case (int)3: {
+			oss << (int)(((uint16_t*)pointer)[0]);
+		}
+		;break;
+		case (int)4: {
+			oss << ((int32_t*)pointer)[0];
+		}
+		;break;
+		case (int)5: {
+			oss << ((uint32_t*)pointer)[0];
+		}
+		;break;
+		case (int)6: {
+			oss << ((float*)pointer)[0];
+		}
+		;break;
+	}
+	return oss.str();
+}
+
+static inline void staticWriteFloatIntoPointer(void *pointer, float value, types::DataType &dataType)
+{
+	switch( (int)(dataType->__Index())){
+		case (int)0: {
+			((int8_t*)pointer)[0] = (int8_t)value;
+		}
+		;break;
+		case (int)1: {
+			((uint8_t*)pointer)[0] = (uint8_t)value;
+		}
+		;break;
+		case (int)2: {
+			((int16_t *)pointer)[0] = (uint16_t)value;
+		}
+		;break;
+		case (int)3: {
+			((uint16_t *)pointer)[0] = (int16_t)value;
+		}
+		;break;
+		case (int)4: {
+			((int32_t *)pointer)[0] = (int32_t)value;
+		}
+		;break;
+		case (int)5: {
+			((uint32_t *)pointer)[0] = (uint32_t)value;
+		}
+		;break;
+		case (int)6: {
+			((float *)pointer)[0] = (float)value;
+		}
+		;break;
+	}
+}
+
+static inline void staticWriteIntIntoPointer(void *pointer, int &value, types::DataType &dataType)
+{
+	switch( (int)(dataType->__Index())){
+		case (int)0: {
+			((int8_t*)pointer)[0] = (int8_t)value;
+		}
+		;break;
+		case (int)1: {
+			((uint8_t*)pointer)[0] = (uint8_t)value;
+		}
+		;break;
+		case (int)2: {
+			((int16_t *)pointer)[0] = (uint16_t)value;
+		}
+		;break;
+		case (int)3: {
+			((uint16_t *)pointer)[0] = (int16_t)value;
+		}
+		;break;
+		case (int)4: {
+			((int32_t *)pointer)[0] = (int32_t)value;
+		}
+		;break;
+		case (int)5: {
+			((uint32_t *)pointer)[0] = (uint32_t)value;
+		}
+		;break;
+		case (int)6: {
+			((float *)pointer)[0] = (float)value;
+		}
+		;break;
+	}
+}
+
+') 
+
+@:headerClassCode('					
+public:								
+	void *_dataPointer;				
+') 
+class Data
 {
 	public var length(default, null) : Int;
 
 	public function new(sizeInBytes : Int) : Void
 	{
 		length = sizeInBytes;
+
+		setupDataPointer();
+		setFinalizer();
 	}
 
+	@:functionCode("
+		hx::GCSetFinalizer(this, (hx::finalizer)staticClose);
+	") 
+	function setFinalizer() {} 
+
+	@:functionCode("
+		_dataPointer = (void*)calloc(length, 1);
+	") 
+	private function setupDataPointer() {}
+
+
+	@:functionCode("
+		memcpy((uint8_t*)_dataPointer + offsetInBytes, data->_dataPointer, data->length);
+	") 
 	public function setData(data : Data, offsetInBytes : Int)
 	{
-
 	}
 
-	public function setValue(value : Dynamic, offsetInBytes : Int, dataType : DataType)
-	{
+	public function setIntArray(array : Array<Int>, offsetInBytes : Int, dataType : DataType) 
+	{ 
+		var dataSize = types.DataTypeUtils.dataTypeByteSize(dataType);
 
+		var currentOffset = offsetInBytes;
+		for(i in 0...array.length)
+		{
+			setInt(array[i], currentOffset, dataType);
+
+			currentOffset += dataSize;
+		}
 	}
 
-	public function setArray(array : Array<Int>, offsetInBytes : Int, dataType : DataType)
-	{
+	public function setFloatArray(array : Array<Float>, offsetInBytes : Int, dataType : DataType) 
+	{ 
+		var dataSize = types.DataTypeUtils.dataTypeByteSize(dataType);
 
+		var currentOffset = offsetInBytes;
+		for(i in 0...array.length)
+		{
+			setFloat(array[i], currentOffset, dataType);
+
+			currentOffset += dataSize;
+		}
 	}
 
-	public function toString(?dataType : DataType) : String
-	{
-		return "Unimplemented";
-	}
+	@:functionCode('
+		staticWriteIntIntoPointer((uint8_t*)_dataPointer + offsetInBytes, value, targetDataType);
+	') 
+	public function setInt(value : Int, offsetInBytes : Int, targetDataType : DataType) {}
+
+	@:functionCode('
+		staticWriteFloatIntoPointer((uint8_t*)_dataPointer + offsetInBytes, value, targetDataType);
+	') 
+	public function setFloat(value : Float, offsetInBytes : Int, targetDataType : DataType) {}
+
+
+	@:functionCode('
+		if(dataType == 0)
+			dataType = ::types::DataType_obj::Int;
+		int dataSize = ::types::DataTypeUtils_obj::dataTypeByteSize(dataType);
+
+		std::wostringstream oss;
+
+		oss << "[";
+		if(length >= 1)
+		{
+			oss << staticReadValueIntoString(_dataPointer, dataType);
+		}
+
+		for(int i = 1; i < length / dataSize; i++)
+		{
+			oss << ", ";
+			oss << staticReadValueIntoString((uint8_t*)_dataPointer + i * dataSize, dataType);
+		}
+
+		oss << "]";
+
+		std::wstring str = oss.str();
+		
+		return ::String(str.c_str(), str.size() + 1);
+	') 
+	public function toString(?dataType : DataType) : String { return "";}
 
 }
-/*
-// This is so the "FILE *" type is known to the compiler.  Other libraries  may require other includes. 
-@:headerCode("#include <stdio.h>") 
-// Define the GC finalizer for method 1 - code goes in the cpp file ..... 
-@:cppFileCode('void staticClose(MyStdio_obj *inObj) { if (inObj->mFile)   
-fclose(inObj->mFile); printf("CLOSED!\\n"); }\n') 
-// Put a "FILE *" member in the class, as well as an inline function. 
-// All members get "memset" to 0 on construction - no automatic destructor will get called... 
-@:headerClassCode('FILE *mFile;\ninline static void Print() {   printf("PRINT!\\n"); }') 
-class MyStdio 
-{ 
-    public function new() 
-    { 
-       // You can sometimes call/use the native members directly if the   usage matches standard 
-       //  haxe usage.  Use the 'untyped' keyword so that the compiler does   not check 
-       //  that MyStdio has a "Print" function. 
-       untyped MyStdio.Print(); 
-       // Can't do code-injection on constructor - must use separate   
-function 
-       setFinalizer(); 
-    } 
-
-    @:functionCode("hx::GCSetFinalizer(this, (hx::finalizer)staticClose);") 
-    function setFinalizer() { } 
-
-    @:functionCode('mFile=::fopen(filename.__s, mode.__s); return   mFile!=0;') 
-    public function fopen(filename:String, mode:String)   { return false;  } 
-
-    @:functionCode('return fwrite(inString.__s, inString.length, 1,   mFile);') 
-    public function writeString(inString:String)  { return 0; } 
-
-    @:functionCode('if (mFile) fclose(mFile); mFile=0;') 
-    public function close() 
-    { 
-       // Ok to do memory operations here (not a native finalizer) 
-       trace("Closed!"); 
-    } 
-} 
-*/
--- types/Matrix.hx
+++ types/Matrix.hx
@@ -2,38 +2,62 @@ package types;
 
 import Data;
 
-extern Class Matrix
+Class Matrix
 {
-	public function new() : Void;
+	public function new() : Void
+	{
+
+	}
 	
 	public static function createOrtho( x0 : Float, 
 										x1 : Float, 
 										y0 : Float, 
 										y1 : Float, 
 										zNear : Float, 
-										zFar : Float) : Matrix;
+										zFar : Float) : Matrix
+	{
+
+	}
 
 	public function setOrtho(	x0 : Float, 
 								x1 : Float, 
 								y0 : Float, 
 								y1 : Float, 
 								zNear : Float, 
-								zFar : Float) : Void;
+								zFar : Float) : Void
+	{
+
+	}
 
 	public static function create2D(posX : Float, 
 									posY : Float, 
 									scale : Float, 
-									rotation : Float) : Matrix;
+									rotation : Float) : Matrix
+	{
+
+	}
 
 	public function set2D(	posX : Float, 
 							posY : Float, 
 							scale : Float, 
-							rotation : Float) : Void;
+							rotation : Float) : Void
+	{
 
-	public static function createBymultiplication(left : Matrix, right : Matrix) : Matrix;
+	}
 
-	public function multiply(right : Matrix) : Void;
+	public static function createBymultiplication(left : Matrix, right : Matrix) : Matrix
+	{
 
-	public var data(default, null) : Data;
+	}
+
+	public function multiply(right : Matrix) : Void
+	{
+
+	}
+
+	public var data(default, null) : Data
+	{
+		
+	}
 
 }
