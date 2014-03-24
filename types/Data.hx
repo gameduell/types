569fc52c3b8468b03ceb9e497db2205e272b2b20
Rui Campos <rui.campos@gameduell.de>
2014-03-24 13:37:59 +0100

    INFRA-2906 implemented types for html5

--- types/Data.hx
+++ types/Data.hx
@@ -1,55 +1,243 @@
 package types;
 
 import types.DataType;
-
+import js.html.ArrayBuffer;
+import js.html.ArrayBufferView;
+import js.html.Int8Array;
+import js.html.Uint8Array;
+import js.html.Int16Array;
+import js.html.Uint16Array;
+import js.html.Int32Array;
+import js.html.Uint32Array;
+import js.html.Float32Array;
+import js.html.Float64Array;
 
 class Data
 {
 	public var length(default, null) : Int;
 
+	public var arrayBuffer(default, set) : ArrayBuffer;
+
+	public var int8Array : Int8Array;
+	public var uint8Array : Uint8Array;
+	public var int16Array : Int16Array;
+	public var uint16Array : Uint16Array;
+	public var int32Array : Int32Array;
+	public var uint32Array : Uint32Array;
+	public var float32Array : Float32Array;
+
 	public function new(sizeInBytes : Int) : Void
 	{
+		if(sizeInBytes != 0)
+		{
+			arrayBuffer = new ArrayBuffer(sizeInBytes);
+		}
 
+		length = sizeInBytes;
 	}
 
-	function setFinalizer() 
+	///for usage in html5 haxelibs
+	public function set_arrayBuffer(value : ArrayBuffer)
 	{
+		var length = value.byteLength;
 
-	} 
+		int8Array = new Int8Array(value);
+	 	uint8Array = new Uint8Array(value);
+	 	if(length % 2  == 0)
+	 	{
+	 		int16Array = new Int16Array(value);
+	 		uint16Array = new Uint16Array(value);
+	 	}
+	 	if(length % 4  == 0)
+	 	{
+	 		int32Array = new Int32Array(value);
+	 		uint32Array = new Uint32Array(value);
+	 		float32Array = new Float32Array(value);
+	 	}
 
-	private function setupDataPointer() 
-	{
+	 	arrayBuffer = value;
 
+		return value;
 	}
 
 	public function setData(data : Data, offsetInBytes : Int)
 	{
-
+		uint8Array.set(data.uint8Array, offsetInBytes);
 	}
+
+	public function getInt(offsetInBytes : Int, targetDataType : DataType) : Int
+	{
+		switch(targetDataType)
+		{
+			case DataTypeByte:
+				return int8Array[offsetInBytes];
+			case DataTypeUnsignedByte:
+				return uint8Array[offsetInBytes];
+			case DataTypeShort:
+				return int16Array[cast offsetInBytes / 2];
+			case DataTypeUnsignedShort:
+				return uint16Array[cast offsetInBytes / 2];
+			case DataTypeInt:
+				return int32Array[cast offsetInBytes / 4];
+			case DataTypeUnsignedInt:
+				return uint32Array[cast offsetInBytes / 4];
+			case DataTypeFloat:
+				return cast float32Array[cast offsetInBytes / 4];
+		}
+		return 0;
+	}
+
+	public function getFloat(offsetInBytes : Int, targetDataType : DataType) : Float
+	{
+		switch(targetDataType)
+		{
+			case DataTypeByte:
+				return cast int8Array[offsetInBytes];
+			case DataTypeUnsignedByte:
+				return cast uint8Array[offsetInBytes];
+			case DataTypeShort:
+				return cast int16Array[cast offsetInBytes / 2];
+			case DataTypeUnsignedShort:
+				return cast uint16Array[cast offsetInBytes / 2];
+			case DataTypeInt:
+				return cast int32Array[cast offsetInBytes / 4];
+			case DataTypeUnsignedInt:
+				return cast uint32Array[cast offsetInBytes / 4];
+			case DataTypeFloat:
+				return cast float32Array[cast offsetInBytes / 4];
+		}
+		return 0;
+	}
+
 
 	public function setIntArray(array : Array<Int>, offsetInBytes : Int, dataType : DataType) 
 	{ 
+		var dataSize = types.DataTypeUtils.dataTypeByteSize(dataType);
 
+		var currentOffset = offsetInBytes;
+		for(i in 0...array.length)
+		{
+			setInt(array[i], currentOffset, dataType);
+
+			currentOffset += dataSize;
+		}
 	}
 
 	public function setFloatArray(array : Array<Float>, offsetInBytes : Int, dataType : DataType) 
 	{ 
+		var dataSize = types.DataTypeUtils.dataTypeByteSize(dataType);
 
+		var currentOffset = offsetInBytes;
+		for(i in 0...array.length)
+		{
+			setFloat(array[i], currentOffset, dataType);
+
+			currentOffset += dataSize;
+		}
 	}
 
+	static var intArrayOf1 : Array<Int> = [0];
 	public function setInt(value : Int, offsetInBytes : Int, targetDataType : DataType) 
 	{
-
+		intArrayOf1[0] = value;
+		switch(targetDataType)
+		{
+			case DataTypeByte:
+				int8Array.set(intArrayOf1, offsetInBytes);
+				return; 
+			case DataTypeUnsignedByte:
+				uint8Array.set(intArrayOf1, offsetInBytes);
+				return;
+			case DataTypeShort:
+				int16Array.set(intArrayOf1, cast offsetInBytes / 2);
+				return;
+			case DataTypeUnsignedShort:
+				uint16Array.set(intArrayOf1, cast offsetInBytes / 2);
+				return;
+			case DataTypeInt:
+				int32Array.set(intArrayOf1, cast offsetInBytes / 4);
+				return;
+			case DataTypeUnsignedInt:
+				uint32Array.set(intArrayOf1, cast offsetInBytes / 4);
+				return;
+			case DataTypeFloat:
+				float32Array.set(intArrayOf1, cast offsetInBytes / 4);
+				return;
+		}
+		return;
 	}
 
 	public function setFloat(value : Float, offsetInBytes : Int, targetDataType : DataType) 
-	{
-
+	{	
+		intArrayOf1[0] = cast value;
+		switch(targetDataType)
+		{
+			case DataTypeByte:
+				int8Array.set(intArrayOf1, offsetInBytes);
+				return; 
+			case DataTypeUnsignedByte:
+				uint8Array.set(intArrayOf1, offsetInBytes);
+				return;
+			case DataTypeShort:
+				int16Array.set(intArrayOf1, cast offsetInBytes / 2);
+				return;
+			case DataTypeUnsignedShort:
+				uint16Array.set(intArrayOf1, cast offsetInBytes / 2);
+				return;
+			case DataTypeInt:
+				int32Array.set(intArrayOf1, cast offsetInBytes / 4);
+				return;
+			case DataTypeUnsignedInt:
+				uint32Array.set(intArrayOf1, cast offsetInBytes / 4);
+				return;
+			case DataTypeFloat:
+				float32Array.set(intArrayOf1, cast offsetInBytes / 4);
+				return;
+		}
+		return;
 	}
 
 	public function toString(?dataType : DataType) : String
 	{ 
-		return "";
+		var output = "";
+		output += "[";
+
+		var view : Dynamic;
+		switch(dataType)
+		{
+			case DataTypeByte:
+				view = int8Array;
+			case DataTypeUnsignedByte:
+				view = uint8Array;
+			case DataTypeShort:
+				view = uint16Array;
+			case DataTypeUnsignedShort:
+				view = uint16Array;
+			case DataTypeInt:
+				view = int32Array;
+			case DataTypeUnsignedInt:
+				view = uint32Array;
+			case DataTypeFloat:
+				view = float32Array;
+		}
+
+		var dataSize = DataTypeUtils.dataTypeByteSize(dataType);
+
+		var count : Int = cast arrayBuffer.byteLength / dataSize;
+
+		if(count > 0)
+		{
+			output += view[0];
+		}
+
+		for(i in 1...count)
+		{
+			output += ", ";
+			output += view[i];
+		}
+
+		output += "]";
+		return output;
 	}
 
 }
--- types/Matrix4.hx
+++ types/Matrix4.hx
@@ -1,24 +1,30 @@
 package types;
 
 import types.Data;
+import types.DataType;
 
 
 class Matrix4
 {
 	public var data(default, null) : Data;
 
+	private static var identity : Data;
+
 	public function new() : Void{
-
-	}
-
-	public function initDataWithMatrixPointer() 
-	{
-
+		data = new Data(4*4*4);
 	}
 
 	public function setIdentity() : Void 
 	{
-
+		if(identity == null)
+		{
+			identity = new Data(4*4*4);
+			identity.setFloatArray([1.0, 0.0, 0.0, 0.0,
+								    0.0, 1.0, 0.0, 0.0,
+								    0.0, 0.0, 1.0, 0.0,
+								    0.0, 0.0, 0.0, 1.0], 0, DataTypeFloat);
+		}
+		data.setData(identity, 0);
 	}
 
 	public function setOrtho(	x0 : Float, 
@@ -28,7 +34,31 @@ class Matrix4
 								zNear : Float, 
 								zFar : Float) : Void 
 	{
+		var floatView = data.float32Array;
 
+    	var ral = x1 + x0;
+    	var rsl = x1 - x0;
+    	var tab = y1 + y0;
+    	var tsb = y1 - y0;
+    	var fan = zFar + zNear;
+    	var fsn = zFar - zNear;
+
+    	floatView[0] = 2.0 / rsl;
+    	floatView[1] = 0.0;
+    	floatView[2] = 0.0;
+    	floatView[3] = 0.0;
+    	floatView[4] = 0.0;
+    	floatView[5] = 2.0 / tsb;
+    	floatView[6] = 0.0;
+    	floatView[7] = 0.0;
+    	floatView[8] = 0.0;
+    	floatView[9] = 0.0;
+    	floatView[10] = -2.0 / fsn;
+    	floatView[11] = 0.0;
+    	floatView[12] = -ral / rsl;
+    	floatView[13] = -tab / tsb;
+    	floatView[14] = -fan / fsn;
+    	floatView[15] = 1.0;
 	}
 
 	public function set2D(	posX : Float, 
@@ -36,22 +66,91 @@ class Matrix4
 							scale : Float, 
 							rotation : Float) : Void 
 	{ 
+		var floatView = data.float32Array;
 
+		var theta = rotation * Math.PI / 180.0;
+      	var c = Math.cos(theta);
+      	var s = Math.sin(theta);
+
+    	floatView[0] = c * scale;
+    	floatView[1] = -s * scale;
+    	floatView[2] = 0.0;
+    	floatView[3] = 0.0;
+    	floatView[4] = s * scale;
+    	floatView[5] = c * scale;
+    	floatView[6] = 0.0;
+    	floatView[7] = 0.0;
+    	floatView[8] = 0.0;
+    	floatView[10] = 1.0;
+    	floatView[11] = 0.0;
+    	floatView[12] = posX;
+    	floatView[13] = posY;
+    	floatView[14] = 0.0;
+    	floatView[15] = 1.0;
 	}
 
 	public function set(matrix : Matrix4) : Void 
 	{ 
+		data.setData(matrix.data, 0);
+	}
 
+	public function get(row : Int, col : Int) : Float
+	{
+		return data.float32Array[row * 4 + col];
 	}
 
 	public function multiply(right : Matrix4) : Void 
 	{ 
+		var a = data.float32Array;
+		var b = right.data.float32Array;
+		var out = data.float32Array;
+   		var a00 = a[0], a01 = a[1], a02 = a[2], a03 = a[3],
+        	a10 = a[4], a11 = a[5], a12 = a[6], a13 = a[7],
+       		a20 = a[8], a21 = a[9], a22 = a[10], a23 = a[11],
+        	a30 = a[12], a31 = a[13], a32 = a[14], a33 = a[15];
 
+	    // Cache only the current line of the second matrix
+	    var b0  = b[0], b1 = b[1], b2 = b[2], b3 = b[3];  
+	    	out[0] = b0*a00 + b1*a10 + b2*a20 + b3*a30;
+	    	out[1] = b0*a01 + b1*a11 + b2*a21 + b3*a31;
+	    	out[2] = b0*a02 + b1*a12 + b2*a22 + b3*a32;
+	    	out[3] = b0*a03 + b1*a13 + b2*a23 + b3*a33;
+
+	    b0 = b[4]; b1 = b[5]; b2 = b[6]; b3 = b[7];
+	    out[4] = b0*a00 + b1*a10 + b2*a20 + b3*a30;
+	    out[5] = b0*a01 + b1*a11 + b2*a21 + b3*a31;
+	    out[6] = b0*a02 + b1*a12 + b2*a22 + b3*a32;
+	    out[7] = b0*a03 + b1*a13 + b2*a23 + b3*a33;
+
+	    b0 = b[8]; b1 = b[9]; b2 = b[10]; b3 = b[11];
+	    out[8] = b0*a00 + b1*a10 + b2*a20 + b3*a30;
+	    out[9] = b0*a01 + b1*a11 + b2*a21 + b3*a31;
+	    out[10] = b0*a02 + b1*a12 + b2*a22 + b3*a32;
+	    out[11] = b0*a03 + b1*a13 + b2*a23 + b3*a33;
+
+	    b0 = b[12]; b1 = b[13]; b2 = b[14]; b3 = b[15];
+	    out[12] = b0*a00 + b1*a10 + b2*a20 + b3*a30;
+	    out[13] = b0*a01 + b1*a11 + b2*a21 + b3*a31;
+	    out[14] = b0*a02 + b1*a12 + b2*a22 + b3*a32;
+	    out[15] = b0*a03 + b1*a13 + b2*a23 + b3*a33;
 	}
 
-	public function toString() : String 
+	public function toString() : String
 	{ 
-		return ""; 
+		var output = "";
+		output += "[";
+
+		var view = data.float32Array;
+		output += view[0];
+
+		for(i in 1...16)
+		{
+			output += ", ";
+			output += view[i];
+		}
+
+		output += "]";
+		return output;
 	}
 
 
