753ff118bf2fc21ed88fd601daea9e309878aaf4
Rui Campos <rui.campos@gameduell.de>
2014-04-08 16:46:24 +0200

    Changed data usage to support inherent pointer offset usage

--- types/Data.hx
+++ types/Data.hx
@@ -6,21 +6,27 @@ extern class Data
 {
 	public function new(sizeInBytes : Int) : Void; /// if 0, empty data, does not create the underlying memory. Can be set externally.
 
-	public function setData(data : Data, offsetInBytes : Int, lengthInBytes : Int) : Void;
+	public function setData(data : Data) : Void;
 
-	public function setInt(value : Int, offsetInBytes : Int, targetDataTyoe : DataType) : Void;
+	public function setInt(value : Int, targetDataTyoe : DataType) : Void;
 
-	public function setFloat(value : Float, offsetInBytes : Int, targetDataTyoe : DataType) : Void;
+	public function setFloat(value : Float, targetDataTyoe : DataType) : Void;
 
-	public function setIntArray(array : Array<Int>, offsetInBytes : Int, dataType : DataType) : Void;
+	public function setIntArray(array : Array<Int>, dataType : DataType) : Void;
 
-	public function setFloatArray(array : Array<Float>, offsetInBytes : Int, dataType : DataType) : Void;
+	public function setFloatArray(array : Array<Float>, dataType : DataType) : Void;
 
-	public function getInt(offsetInBytes : Int, targetDataType : DataType) : Int;
+	public function getInt(targetDataType : DataType) : Int;
 
-	public function getFloat(offsetInBytes : Int, targetDataType : DataType) : Float;
+	public function getFloat(targetDataType : DataType) : Float;
 
 	public function toString(?dataType : DataType) : String; /// defaults to DataType.Int
 
-	public var length(default, null) : Int;
+	public var allocedLength(get, never) : Int;
+
+
+	/// offset view, all uses of data should start at offset and use up to offset length
+	public var offset(get, set) : Int;
+	public var offsetLength(get, set) : Int;
+	public function resetOffset() : Void; ///makes offset 0 and offsetLength be length
 }
