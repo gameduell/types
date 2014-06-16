78f861567b80b8f65a6eeb66f8cf4dd4c66422de
Mad <mad.paia@gmail.com>
2014-06-16 14:06:22 +0200

    Added minor changes

--- types/Data.hx
+++ types/Data.hx
@@ -1,4 +1,5 @@
 package types;
+import flash.utils.Endian;
 import flash.utils.ByteArray;
 import types.DataType;
 
@@ -55,6 +56,7 @@ class Data
         {
             _offsetLength = sizeInBytes;
             byteArray = new ByteArray();
+            byteArray.endian = Endian.LITTLE_ENDIAN;
             byteArray.length = sizeInBytes;
             byteArray.position = 0;
         }
@@ -70,192 +72,280 @@ class Data
     public function writeData(data : Data) : Void
     {
         var prevOffset = byteArray.position;
-        byteArray.writeBytes(data.byteArray, data.offset, data.byteArray.length);
-        offset = prevOffset;
+        byteArray.writeBytes(data.byteArray, 0, data.byteArray.length);
+        byteArray.position = prevOffset;
+    }
+
+    public function readIntArray(length:Int, targetDataType : DataType) : Array<Int>
+    {
+        var dataSize = types.DataTypeUtils.dataTypeByteSize(targetDataType);
+        var prevOffset = byteArray.position;
+        var returnArray = new Array<Int>();
+
+        for(i in 0...length)
+        {
+            byteArray.position = prevOffset + (i * dataSize);
+            returnArray.push(readInt(targetDataType));
+        }
+
+        byteArray.position = prevOffset;
+
+        return returnArray;
+    }
+
+    public function readUIntArray(targetDataType : DataType) : Array<UInt>
+    {
+        var dataSize = types.DataTypeUtils.dataTypeByteSize(targetDataType);
+        var returnArray = new Array<UInt>();
+        var prevOffset = byteArray.position;
+        var length:Int  = cast byteArray.length / dataSize;
+
+        for(i in 0...length)
+        {
+            byteArray.position = prevOffset + (i * dataSize);
+            returnArray.push(readInt(targetDataType));
+        }
+
+        byteArray.position = prevOffset;
+
+        return returnArray;
+    }
+
+    public function readFloatArray(targetDataType : DataType) : Array<Float>
+    {
+        var dataSize = types.DataTypeUtils.dataTypeByteSize(targetDataType);
+        var prevOffset = byteArray.position;
+        var returnArray = new Array<Float>();
+        var length:Int  = cast byteArray.length / dataSize;
+
+        for(i in 0...length)
+        {
+            byteArray.position = prevOffset + (i * dataSize);
+            returnArray.push(readFloat(targetDataType));
+        }
+
+        byteArray.position = prevOffset;
+
+        return returnArray;
     }
 
     public function readInt(targetDataType : DataType) : Int
     {
         var prevOffset = byteArray.position;
-        var returnValue = 0;
+        var returnValue:Int;
+
         switch(targetDataType)
         {
-            case DataTypeByte:
+            case DataTypeInt8:
                 returnValue =  byteArray.readByte();
-            case DataTypeUnsignedByte:
-                returnValue =  cast byteArray.readUnsignedByte();
-            case DataTypeShort:
+
+            case DataTypeUInt8:
+                returnValue =  byteArray.readUnsignedByte();
+
+            case DataTypeInt16:
                 returnValue =  byteArray.readShort();
-            case DataTypeUnsignedShort:
-                returnValue =  cast byteArray.readUnsignedShort();
-            case DataTypeInt:
+
+            case DataTypeUInt16:
+                returnValue =  byteArray.readUnsignedShort();
+
+            case DataTypeInt32:
                 returnValue =  byteArray.readInt();
-            case DataTypeUnsignedInt:
-                returnValue =  cast byteArray.readUnsignedInt();
-            case DataTypeFloat:
+
+            case DataTypeUInt32:
+                returnValue = byteArray.readUnsignedInt();
+
+            case DataTypeFloat32:
                 returnValue =  Std.int(byteArray.readFloat());
+
+            case DataTypeFloat64:
+                returnValue =  Std.int(byteArray.readDouble());
         }
-        offset = prevOffset;
+
+        byteArray.position = prevOffset;
+
         return returnValue;
     }
 
     public function readFloat(targetDataType : DataType) : Float
     {
         var prevOffset = byteArray.position;
-        var returnValue = 0.0;
+        var returnValue:Float;
+
         switch(targetDataType)
         {
-            case DataTypeByte:
+            case DataTypeInt8:
                 returnValue =  byteArray.readByte();
-            case DataTypeUnsignedByte:
+
+            case DataTypeUInt8:
                 returnValue =  cast byteArray.readUnsignedByte();
-            case DataTypeShort:
+
+            case DataTypeInt16:
                 returnValue =  byteArray.readShort();
-            case DataTypeUnsignedShort:
+
+            case DataTypeUInt16:
                 returnValue =  cast byteArray.readUnsignedShort();
-            case DataTypeInt:
+
+            case DataTypeInt32:
                 returnValue =  byteArray.readInt();
-            case DataTypeUnsignedInt:
+
+            case DataTypeUInt32:
                 returnValue =  cast byteArray.readUnsignedInt();
-            case DataTypeFloat:
+
+            case DataTypeFloat32:
                 returnValue = byteArray.readFloat();
+
+            case DataTypeFloat64:
+                returnValue = byteArray.readDouble();
         }
-        offset = prevOffset;
+
+        byteArray.position = prevOffset;
+
         return returnValue;
     }
-
 
     public function writeIntArray(array : Array<Int>, dataType : DataType) : Void
     {
         var dataSize = types.DataTypeUtils.dataTypeByteSize(dataType);
-
         var prevOffset = byteArray.position;
+
         for(i in 0...array.length)
         {
             byteArray.position = prevOffset + (i * dataSize);
             writeInt(array[i], dataType);
         }
-        offset = prevOffset;
+
+        byteArray.position = prevOffset;
     }
 
     public function writeFloatArray(array : Array<Float>, dataType : DataType) : Void
     {
         var dataSize = types.DataTypeUtils.dataTypeByteSize(dataType);
-
         var prevOffset = byteArray.position;
+
         for(i in 0...array.length)
         {
             byteArray.position = prevOffset + (i * dataSize);
             writeFloat(array[i], dataType);
-
         }
-        offset = prevOffset;
+
+        byteArray.position = prevOffset;
     }
 
     public function writeInt(value : Int, targetDataType : DataType) : Void
     {
         var prevOffset = byteArray.position;
+
         switch(targetDataType)
         {
-            case DataTypeByte:
-                byteArray.writeByte(value);
-                
-            case DataTypeUnsignedByte:
+            case DataTypeInt8:
                 byteArray.writeByte(value);
 
-            case DataTypeShort:
+            case DataTypeUInt8:
+                byteArray.writeByte(value);
+
+            case DataTypeInt16:
                 byteArray.writeShort(value);
 
-            case DataTypeUnsignedShort:
+            case DataTypeUInt16:
                 byteArray.writeShort(value);
 
-            case DataTypeInt:
+            case DataTypeInt32:
                 byteArray.writeInt(value);
 
-            case DataTypeUnsignedInt:
+            case DataTypeUInt32:
                 byteArray.writeUnsignedInt(value);
 
-            case DataTypeFloat:
+            case DataTypeFloat32:
                 byteArray.writeFloat(value);
+
+            case DataTypeFloat64:
+                byteArray.writeDouble(value);
         }
-        offset = prevOffset;
-        return;
+
+        byteArray.position = prevOffset;
     }
 
     public function writeFloat(value : Float, targetDataType : DataType) : Void
     {
         var prevOffset = byteArray.position;
+
         switch(targetDataType)
         {
-            case DataTypeByte:
+            case DataTypeInt8:
                 byteArray.writeByte(Std.int(value));
 
-            case DataTypeUnsignedByte:
+            case DataTypeUInt8:
                 byteArray.writeByte(Std.int(value));
 
-            case DataTypeShort:
+            case DataTypeInt16:
                 byteArray.writeShort(Std.int(value));
 
-            case DataTypeUnsignedShort:
+            case DataTypeUInt16:
                 byteArray.writeShort(Std.int(value));
 
-            case DataTypeInt:
+            case DataTypeInt32:
                 byteArray.writeInt(Std.int(value));
 
-            case DataTypeUnsignedInt:
+            case DataTypeUInt32:
                 byteArray.writeUnsignedInt(Std.int(value));
 
-            case DataTypeFloat:
+            case DataTypeFloat32:
                 byteArray.writeFloat(value);
+
+            case DataTypeFloat64:
+                byteArray.writeDouble(value);
         }
-        offset = prevOffset;
-        return;
+
+        byteArray.position = prevOffset;
     }
 
     public function toString(?dataType : DataType = null) : String
     {
         if(dataType == null)
         {
-            dataType = DataTypeInt;
+            dataType = DataTypeInt32;
         }
 
         var dataTypeSize : Int = DataTypeUtils.dataTypeByteSize(dataType);
-
         var prevPosition : Int = byteArray.position;
-        byteArray.position = 0;
-
         var func: types.DataType -> Float;
 
-        if(dataType == DataTypeFloat){
+        if(dataType == DataTypeFloat32 || dataType == DataTypeFloat64){
             func = readFloat;
         }else{
             func = readInt;
         }
 
         var returnString:String = "";
+        var nextPosition : Int;
+        byteArray.position = 0;
+
         while (byteArray.bytesAvailable >0) {
 
-            var nextPosition : Int = byteArray.position + dataTypeSize;
-
+            nextPosition = byteArray.position + dataTypeSize;
             returnString += func(dataType);
+
             byteArray.position = nextPosition;
 
             if(byteArray.bytesAvailable>0)returnString += ",";
-
         }
 
-        offset = prevPosition;
+        byteArray.position = prevPosition;
+
         return returnString;
     }
 
     public function resize(newSize : Int) : Void
     {
         var prevPosition : Int = byteArray.position;
+
         var newBuffer:ByteArray = new ByteArray();
         newBuffer.length = newSize;
         newBuffer.writeBytes(byteArray, 0, byteArray.length);
-        newBuffer.position = prevPosition;
         _offsetLength = newSize;
+
+        newBuffer.position = prevPosition;
+
         set_byteArray(newBuffer);
     }
 
--- types/Matrix4.hx
+++ types/Matrix4.hx
@@ -17,12 +17,14 @@ class Matrix4
         {
             identity = new Data(4*4*4);
             identity.offset = 0;
-            identity.writeFloatArray([
+            identity.writeFloatArray(
+                [
             1.0, 0.0, 0.0, 0.0,
             0.0, 1.0, 0.0, 0.0,
             0.0, 0.0, 1.0, 0.0,
-            0.0, 0.0, 0.0, 1.0]
-            , DataTypeFloat);
+            0.0, 0.0, 0.0, 1.0
+                ]
+            , DataTypeFloat32);
         }
 
         data.offset = 0;
@@ -32,7 +34,7 @@ class Matrix4
 
     public function setOrtho( x0 : Float, x1 : Float, y0 : Float, y1 : Float, zNear : Float, zFar : Float) : Void
     {
-
+        var oldOffset = data.offset;
         var ral:Float = x1 + x0;
         var rsl:Float = x1 - x0;
         var tab:Float = y1 + y0;
@@ -42,226 +44,230 @@ class Matrix4
         var counter:Int = 0;
 
         data.offset = counter;
-        data.writeFloat(2.0 / rsl, DataTypeFloat);       //0
+        data.writeFloat(2.0 / rsl, DataTypeFloat32);       //0
         data.offset = dataSize * ++counter;
-        data.writeFloat(0.0, DataTypeFloat);             //1
+        data.writeFloat(0.0, DataTypeFloat32);             //1
         data.offset = dataSize * ++counter;
-        data.writeFloat(0.0, DataTypeFloat);             //2
+        data.writeFloat(0.0, DataTypeFloat32);             //2
         data.offset = dataSize * ++counter;
-        data.writeFloat(0.0, DataTypeFloat);             //3
+        data.writeFloat(0.0, DataTypeFloat32);             //3
         data.offset = dataSize * ++counter;
-        data.writeFloat(0.0, DataTypeFloat);             //4
+        data.writeFloat(0.0, DataTypeFloat32);             //4
         data.offset = dataSize * ++counter;
-        data.writeFloat(2.0 / tsb, DataTypeFloat);       //5
+        data.writeFloat(2.0 / tsb, DataTypeFloat32);       //5
         data.offset = dataSize * ++counter;
-        data.writeFloat(0.0, DataTypeFloat);             //6
+        data.writeFloat(0.0, DataTypeFloat32);             //6
         data.offset = dataSize * ++counter;
-        data.writeFloat(0.0, DataTypeFloat);             //7
+        data.writeFloat(0.0, DataTypeFloat32);             //7
         data.offset = dataSize * ++counter;
-        data.writeFloat(0.0, DataTypeFloat);             //8
+        data.writeFloat(0.0, DataTypeFloat32);             //8
         data.offset = dataSize * ++counter;
-        data.writeFloat(0.0, DataTypeFloat);             //9
+        data.writeFloat(0.0, DataTypeFloat32);             //9
         data.offset = dataSize * ++counter;
-        data.writeFloat(-2.0 / fsn, DataTypeFloat);      //10
+        data.writeFloat(-2.0 / fsn, DataTypeFloat32);      //10
         data.offset = dataSize * ++counter;
-        data.writeFloat(0.0, DataTypeFloat);             //11
+        data.writeFloat(0.0, DataTypeFloat32);             //11
         data.offset = dataSize * ++counter;
-        data.writeFloat(-ral / rsl, DataTypeFloat);      //12
+        data.writeFloat(-ral / rsl, DataTypeFloat32);      //12
         data.offset = dataSize * ++counter;
-        data.writeFloat(-tab / tsb, DataTypeFloat);      //13
+        data.writeFloat(-tab / tsb, DataTypeFloat32);      //13
         data.offset = dataSize * ++counter;
-        data.writeFloat(-fan / fsn, DataTypeFloat);      //14
+        data.writeFloat(-fan / fsn, DataTypeFloat32);      //14
         data.offset = dataSize * ++counter;
-        data.writeFloat(1.0, DataTypeFloat);             //15
+        data.writeFloat(1.0, DataTypeFloat32);             //15
 
+        data.offset = oldOffset;
     }
 
     public function set2D( posX : Float, posY : Float, scale : Float,rotation : Float) : Void {
-
+        var oldOffset = data.offset;
         var theta = rotation * Math.PI / 180.0;
         var c = Math.cos(theta);
         var s = Math.sin(theta);
         var counter:Int = 0;
 
         data.offset = counter;
-        data.writeFloat( c * scale,     DataTypeFloat);
+        data.writeFloat( c * scale,     DataTypeFloat32);
         data.offset = dataSize * ++counter;
-        data.writeFloat( -s * scale,    DataTypeFloat);
+        data.writeFloat( -s * scale,    DataTypeFloat32);
         data.offset = dataSize * ++counter;
-        data.writeFloat( 0.0,           DataTypeFloat);
+        data.writeFloat( 0.0,           DataTypeFloat32);
         data.offset = dataSize * ++counter;
-        data.writeFloat( 0.0,           DataTypeFloat);
+        data.writeFloat( 0.0,           DataTypeFloat32);
         data.offset = dataSize * ++counter;
-        data.writeFloat( s * scale,     DataTypeFloat);
+        data.writeFloat( s * scale,     DataTypeFloat32);
         data.offset = dataSize * ++counter;
-        data.writeFloat( c * scale,     DataTypeFloat);
+        data.writeFloat( c * scale,     DataTypeFloat32);
         data.offset = dataSize * ++counter;
-        data.writeFloat( 0.0,           DataTypeFloat);
+        data.writeFloat( 0.0,           DataTypeFloat32);
         data.offset = dataSize * ++counter;
-        data.writeFloat( 0.0,           DataTypeFloat);
+        data.writeFloat( 0.0,           DataTypeFloat32);
         data.offset = dataSize * ++counter;
-        data.writeFloat( 0.0,           DataTypeFloat);
+        data.writeFloat( 0.0,           DataTypeFloat32);
         data.offset = dataSize * ++counter;
-        data.writeFloat( 0.0,           DataTypeFloat);
+        data.writeFloat( 0.0,           DataTypeFloat32);
         data.offset = dataSize * ++counter;
-        data.writeFloat( 1.0,           DataTypeFloat);
+        data.writeFloat( 1.0,           DataTypeFloat32);
         data.offset = dataSize * ++counter;
-        data.writeFloat( 0.0,           DataTypeFloat);
+        data.writeFloat( 0.0,           DataTypeFloat32);
         data.offset = dataSize * ++counter;
-        data.writeFloat( posX,          DataTypeFloat);
+        data.writeFloat( posX,          DataTypeFloat32);
         data.offset = dataSize * ++counter;
-        data.writeFloat( posY,          DataTypeFloat);
+        data.writeFloat( posY,          DataTypeFloat32);
         data.offset = dataSize * ++counter;
-        data.writeFloat( 0.0,           DataTypeFloat);
+        data.writeFloat( 0.0,           DataTypeFloat32);
         data.offset = dataSize * ++counter;
-        data.writeFloat( 1.0,           DataTypeFloat);
+        data.writeFloat( 1.0,           DataTypeFloat32);
 
-        data.offset = 0;
+        data.offset = oldOffset;
     }
 
     public function set(other : Matrix4) : Void {
+        var prevOffset = data.offset;
         data.offset = 0;
         other.data.offset = 0;
         data.writeData(other.data);
+        data.offset = prevOffset;
     }
 
     public function get(row : Int, col : Int) : Float{
-        var prevOffset:Int= data.offset;
+        var oldOffset =  data.offset;
         data.offset = (row * 4 + col) * dataSize;
-        var returnValue = data.readFloat(DataTypeFloat);
-        data.offset = prevOffset;
+        var returnValue = data.readFloat(DataTypeFloat32);
+        data.offset = oldOffset;
         return returnValue;
     }
 
     public function multiply(right : Matrix4) : Void{
+        var oldOffset = data.offset;
         var a = data;
         var b = right.data;
         var out = data;
         var counter:Int = 0;
         a.offset = counter;
 
-        var a00 = a.readFloat(DataTypeFloat);
+        var a00 = a.readFloat(DataTypeFloat32);
         a.offset = dataSize * ++counter;
-        var a01 = a.readFloat(DataTypeFloat);
+        var a01 = a.readFloat(DataTypeFloat32);
         a.offset = dataSize * ++counter;
-        var a02 = a.readFloat(DataTypeFloat);
+        var a02 = a.readFloat(DataTypeFloat32);
         a.offset = dataSize * ++counter;
-        var a03 = a.readFloat(DataTypeFloat);
+        var a03 = a.readFloat(DataTypeFloat32);
         a.offset = dataSize * ++counter;
-        var a10 = a.readFloat(DataTypeFloat);
+        var a10 = a.readFloat(DataTypeFloat32);
         a.offset = dataSize * ++counter;
-        var a11 = a.readFloat(DataTypeFloat);
+        var a11 = a.readFloat(DataTypeFloat32);
         a.offset = dataSize * ++counter;
-        var a12 = a.readFloat(DataTypeFloat);
+        var a12 = a.readFloat(DataTypeFloat32);
         a.offset = dataSize * ++counter;
-        var a13 = a.readFloat(DataTypeFloat);
+        var a13 = a.readFloat(DataTypeFloat32);
         a.offset = dataSize * ++counter;
-        var a20 = a.readFloat(DataTypeFloat);
+        var a20 = a.readFloat(DataTypeFloat32);
         a.offset = dataSize * ++counter;
-        var a21 = a.readFloat(DataTypeFloat);
+        var a21 = a.readFloat(DataTypeFloat32);
         a.offset = dataSize * ++counter;
-        var a22 = a.readFloat(DataTypeFloat);
+        var a22 = a.readFloat(DataTypeFloat32);
         a.offset = dataSize * ++counter;
-        var a23 = a.readFloat(DataTypeFloat);
+        var a23 = a.readFloat(DataTypeFloat32);
         a.offset = dataSize * ++counter;
-        var a30 = a.readFloat(DataTypeFloat);
+        var a30 = a.readFloat(DataTypeFloat32);
         a.offset = dataSize * ++counter;
-        var a31 = a.readFloat(DataTypeFloat);
+        var a31 = a.readFloat(DataTypeFloat32);
         a.offset = dataSize * ++counter;
-        var a32 = a.readFloat(DataTypeFloat);
+        var a32 = a.readFloat(DataTypeFloat32);
         a.offset = dataSize * ++counter;
-        var a33 = a.readFloat(DataTypeFloat);
+        var a33 = a.readFloat(DataTypeFloat32);
 
         counter = 0;
         b.offset = counter * dataSize;
         // Cache only the current line of the second matrix
-        var b0 = b.readFloat(DataTypeFloat);
+        var b0 = b.readFloat(DataTypeFloat32);
         b.offset = dataSize * ++counter;
-        var b1 = b.readFloat(DataTypeFloat);
+        var b1 = b.readFloat(DataTypeFloat32);
         b.offset = dataSize * ++counter;
-        var b2 = b.readFloat(DataTypeFloat);
+        var b2 = b.readFloat(DataTypeFloat32);
         b.offset = dataSize * ++counter;
-        var b3 = b.readFloat(DataTypeFloat);
+        var b3 = b.readFloat(DataTypeFloat32);
 
         counter = 0;
         out.offset = counter * dataSize;
 
-        out.writeFloat(b0*a00 + b1*a10 + b2*a20 + b3*a30,DataTypeFloat);
+        out.writeFloat(b0*a00 + b1*a10 + b2*a20 + b3*a30,DataTypeFloat32);
         out.offset = dataSize * ++counter;
-        out.writeFloat(b0*a01 + b1*a11 + b2*a21 + b3*a31,DataTypeFloat);
+        out.writeFloat(b0*a01 + b1*a11 + b2*a21 + b3*a31,DataTypeFloat32);
         out.offset = dataSize * ++counter;
-        out.writeFloat(b0*a02 + b1*a12 + b2*a22 + b3*a32,DataTypeFloat);
+        out.writeFloat(b0*a02 + b1*a12 + b2*a22 + b3*a32,DataTypeFloat32);
         out.offset = dataSize * ++counter;
-        out.writeFloat(b0*a03 + b1*a13 + b2*a23 + b3*a33,DataTypeFloat);
+        out.writeFloat(b0*a03 + b1*a13 + b2*a23 + b3*a33,DataTypeFloat32);
 
         b.offset = dataSize * ++counter;
 
-        b0 = b.readFloat(DataTypeFloat);
+        b0 = b.readFloat(DataTypeFloat32);
         b.offset = dataSize * ++counter;
-        b1 = b.readFloat(DataTypeFloat);
+        b1 = b.readFloat(DataTypeFloat32);
         b.offset = dataSize * ++counter;
-        b2 = b.readFloat(DataTypeFloat);
+        b2 = b.readFloat(DataTypeFloat32);
         b.offset = dataSize * ++counter;
-        b3 = b.readFloat(DataTypeFloat);
+        b3 = b.readFloat(DataTypeFloat32);
 
         counter = 4;
         out.offset = counter * dataSize;
 
-        out.writeFloat(b0*a00 + b1*a10 + b2*a20 + b3*a30,DataTypeFloat);
+        out.writeFloat(b0*a00 + b1*a10 + b2*a20 + b3*a30,DataTypeFloat32);
         out.offset = dataSize * ++counter;
-        out.writeFloat(b0*a01 + b1*a11 + b2*a21 + b3*a31,DataTypeFloat);
+        out.writeFloat(b0*a01 + b1*a11 + b2*a21 + b3*a31,DataTypeFloat32);
         out.offset = dataSize * ++counter;
-        out.writeFloat(b0*a02 + b1*a12 + b2*a22 + b3*a32,DataTypeFloat);
+        out.writeFloat(b0*a02 + b1*a12 + b2*a22 + b3*a32,DataTypeFloat32);
         out.offset = dataSize * ++counter;
-        out.writeFloat(b0*a03 + b1*a13 + b2*a23 + b3*a33,DataTypeFloat);
+        out.writeFloat(b0*a03 + b1*a13 + b2*a23 + b3*a33,DataTypeFloat32);
 
         b.offset = dataSize * ++counter;
 
-        b0 = b.readFloat(DataTypeFloat);
+        b0 = b.readFloat(DataTypeFloat32);
         b.offset = dataSize * ++counter;
-        b1 = b.readFloat(DataTypeFloat);
+        b1 = b.readFloat(DataTypeFloat32);
         b.offset = dataSize * ++counter;
-        b2 = b.readFloat(DataTypeFloat);
+        b2 = b.readFloat(DataTypeFloat32);
         b.offset = dataSize * ++counter;
-        b3 = b.readFloat(DataTypeFloat);
+        b3 = b.readFloat(DataTypeFloat32);
 
         counter = 8;
         out.offset = counter * dataSize;
 
-        out.writeFloat(b0*a00 + b1*a10 + b2*a20 + b3*a30,DataTypeFloat);
+        out.writeFloat(b0*a00 + b1*a10 + b2*a20 + b3*a30,DataTypeFloat32);
         out.offset = dataSize * ++counter;
-        out.writeFloat(b0*a01 + b1*a11 + b2*a21 + b3*a31,DataTypeFloat);
+        out.writeFloat(b0*a01 + b1*a11 + b2*a21 + b3*a31,DataTypeFloat32);
         out.offset = dataSize * ++counter;
-        out.writeFloat(b0*a02 + b1*a12 + b2*a22 + b3*a32,DataTypeFloat);
+        out.writeFloat(b0*a02 + b1*a12 + b2*a22 + b3*a32,DataTypeFloat32);
         out.offset = dataSize * ++counter;
-        out.writeFloat(b0*a03 + b1*a13 + b2*a23 + b3*a33,DataTypeFloat);
+        out.writeFloat(b0*a03 + b1*a13 + b2*a23 + b3*a33,DataTypeFloat32);
 
         b.offset = dataSize * ++counter;
 
-        b0 = b.readFloat(DataTypeFloat);
+        b0 = b.readFloat(DataTypeFloat32);
         b.offset = dataSize * ++counter;
-        b1 = b.readFloat(DataTypeFloat);
+        b1 = b.readFloat(DataTypeFloat32);
         b.offset = dataSize * ++counter;
-        b2 = b.readFloat(DataTypeFloat);
+        b2 = b.readFloat(DataTypeFloat32);
         b.offset = dataSize * ++counter;
-        b3 = b.readFloat(DataTypeFloat);
+        b3 = b.readFloat(DataTypeFloat32);
 
         counter = 12;
         out.offset = counter * dataSize;
 
-        out.writeFloat(b0*a00 + b1*a10 + b2*a20 + b3*a30,DataTypeFloat);
+        out.writeFloat(b0*a00 + b1*a10 + b2*a20 + b3*a30,DataTypeFloat32);
         out.offset = dataSize * ++counter;
-        out.writeFloat(b0*a01 + b1*a11 + b2*a21 + b3*a31,DataTypeFloat);
+        out.writeFloat(b0*a01 + b1*a11 + b2*a21 + b3*a31,DataTypeFloat32);
         out.offset = dataSize * ++counter;
-        out.writeFloat(b0*a02 + b1*a12 + b2*a22 + b3*a32,DataTypeFloat);
+        out.writeFloat(b0*a02 + b1*a12 + b2*a22 + b3*a32,DataTypeFloat32);
         out.offset = dataSize * ++counter;
-        out.writeFloat(b0*a03 + b1*a13 + b2*a23 + b3*a33,DataTypeFloat);
+        out.writeFloat(b0*a03 + b1*a13 + b2*a23 + b3*a33,DataTypeFloat32);
 
         b.offset = 0;
-        out.offset = 0;
+        data.offset = oldOffset;
     }
 
     public function toString() : String{
-        return data.toString(DataTypeFloat);
+        return data.toString(DataTypeFloat32);
     }
 }
