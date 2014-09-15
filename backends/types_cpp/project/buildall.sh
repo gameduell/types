d695332e429934363b9fa35fc523a40c23376af6
Rui Campos <rui.campos@gameduell.de>
2014-09-15 16:16:49 +0200

    removed Touch and MouseEvent

--- backends/types_cpp/project/buildall.sh
+++
@@ -1,9 +0,0 @@
-haxelib run hxcpp Build.xml -Diphonesim
-haxelib run hxcpp Build.xml -Diphoneos
-haxelib run hxcpp Build.xml -Diphoneos -DHXCPP_ARMV7
-haxelib run hxcpp Build.xml -Dmac
-haxelib run hxcpp Build.xml -Dmac -DHXCPP_M64
-haxelib run hxcpp Build.xml -Dmac -Dmacane -Dstatic_link
-haxelib run hxcpp Build.xml -Dmac -Dmacane -DHXCPP_M64 -Dstatic_link
-haxelib run hxcpp Build.xml -Dandroid
-haxelib run hxcpp Build.xml -Dandroid -DHXCPP_ARMV7
