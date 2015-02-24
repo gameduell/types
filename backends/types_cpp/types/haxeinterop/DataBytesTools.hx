77ea5f88156c88a1757279b7ec0dccd649247ae4
Rui Campos <rui.campos@gameduell.de>
2015-02-24 19:09:21 +0100

    fixed memory problem with databytestools

--- backends/types_cpp/types/haxeinterop/DataBytesTools.hx
+++ backends/types_cpp/types/haxeinterop/DataBytesTools.hx
@@ -10,6 +10,31 @@ import haxe.io.BytesData;
 import haxe.io.Bytes;
 import types.Data;
 
+class DataWithHaxeBytes extends Data
+{
+    private var bytesData : BytesData;
+
+    @:functionCode('
+        bytesData = bytes->b;
+        _nativeData->setupWithExistingPointer((uint8_t *)bytes->b->GetBase(), bytes->length);
+        bytes->b = null();
+    ')
+    public function setupWithHaxeBytes(bytes: Bytes): Void {}
+
+    override public function trim(): Void 
+    {
+        bytesData = null;
+        super.trim();
+    }
+
+    override public function resize(newSize: Int): Void 
+    {
+        bytesData = null;
+        super.resize(newSize);
+    }
+
+}
+
 @:headerCode("
 #include <types/NativeData.h>
 ")
@@ -22,15 +47,13 @@ class DataBytesTools
 		memcpy((void *)array->__CStr(), (char*)data->_nativeData->ptr + data->_nativeData->offset, data->get_offsetLength());
         return ::haxe::io::Bytes_obj::__new(data->get_offsetLength(), array);
 	')
-    public static function getBytes(data : Data) : Bytes { return null; }
+    public static function getBytes(data: Data): Bytes { return null; }
 
-    @:functionCode('
-        ::types::Data d = ::types::Data_obj::__new((int)0);
-
-        d->_nativeData->setupWithExistingPointer((uint8_t *)bytes->b->GetBase(), bytes->length);
-
-        return d;
-    ')
-    public static function getTypesData(bytes : Bytes) : Data{ return null; }
+    public static function getTypesData(bytes: Bytes): Data
+    { 
+        var d = new DataWithHaxeBytes(0);
+        d.setupWithHaxeBytes(bytes);
+        return d; 
+    }
 
 }
