c1dcd0a724bca10c2b486377d31c4aabb201a2fc
David Richter <david.richter@gameduell.de>
2015-08-04 15:12:46 +0200

    ci fixed

--- ci.sh
+++ ci.sh
@@ -7,7 +7,7 @@ rm -rf Export
 
 haxelib run duell_duell update -verbose -yestoall
 
-haxelib run duell_duell build android -emulator -test -verbose -D jenkins -yestoall -x86
+haxelib run duell_duell build android -emulator -test -verbose -D jenkins -yestoall -x86 -wipeemulator
 
 haxelib run duell_duell build html5 -test -verbose -D jenkins -yestoall
 
