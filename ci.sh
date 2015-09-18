d435ac59279ed5d7178dda2c9cad2a01310aecf4
Rui Campos <rui.campos@gameduell.de>
2015-09-18 10:13:26 +0200

    removed flash from ci job

--- ci.sh
+++ ci.sh
@@ -11,6 +11,4 @@ haxelib run duell_duell build android -emulator -test -verbose -D jenkins -yesto
 
 haxelib run duell_duell build html5 -test -verbose -D jenkins -yestoall
 
-haxelib run duell_duell build flash -test -verbose -D jenkins -yestoall
-
 haxelib run duell_duell build ios -test -verbose -D jenkins -yestoall
