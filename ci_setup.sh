7474fefecf5599ac9332810489117c72bd82a918
Christian Lührs <christian.luehrs@gameduell.de>
2016-01-14 14:34:32 +0100

    adjustments for ci

--- ci_setup.sh
+++ ci_setup.sh
@@ -15,19 +15,30 @@ rm -rf Export
 printf '\n'
 printf ' ---------- UPDATE DUELL DEPENDENCIES ----------'
 printf '\n'
+printf '\n'
 haxelib run duell_duell update -verbose -yestoall
 
 printf '\n'
 printf ' ---------- BUILD ANDROID BINARY ----------'
+printf '\n'
 printf '\n'
 haxelib run duell_duell build android -norun -verbose -yestoall -x86 -D jenkins
 
 printf '\n'
 printf ' ---------- BUILD IOS BINARY ----------'
 printf '\n'
+printf '\n'
 haxelib run duell_duell build ios -norun -simulator -verbose -yestoall -D jenkins
 
 printf '\n'
 printf ' ---------- BUILD HTML TARGET ----------'
 printf '\n'
+printf '\n'
 haxelib run duell_duell build html5 -norun -verbose -D jenkins -yestoall
+
+cd ..
+printf '\n'
+printf ' ---------- Zipping artifacts ----------'
+printf '\n'
+printf '\n'
+zip -r -X ./tests/Export/artifacts.zip ./tests/Export/html5/web/ ./tests/Export/android/TestProject/build/outputs/apk/TestProject-release.apk ./tests/Export/ios/build/Release-iphonesimulator/TestProject.app
